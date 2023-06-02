01567b26b4909ace83a44f516ea425d509823a97
AJ Djiemuratov <lmt9229@yahoo.com>
2023-06-02 23:02:45 +0500

    select options

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,70 +1,36 @@
 import styles from './App.module.css';
 import { useState } from 'react';
 
-const initialState = {
-	email: '',
-	login: '',
-	password: '',
-};
-
-const useStore = () => {
-	const [state, setState] = useState(initialState);
-
-	return {
-		getState: () => state,
-		updateState: (fieldName, newValue) => {
-			setState({ ...state, [fieldName]: newValue });
-		},
-		resetState: () => setState(initialState),
-	};
-};
-
-const sendData = (formData) => {
-	console.log(formData);
-};
-
 export const App = () => {
-	const { getState, updateState, resetState } = useStore();
+	const [selectedProduct, setSelectedProduct] = useState('tv');
+	const [selectedColors, setSelectedColors] = useState(['blacf', 'silver']);
 
-	const onSubmit = (event) => {
-		event.preventDefault();
-		sendData(getState());
+	const onSelectedProductChange = ({ target }) => setSelectedProduct(target.value);
+
+	const onSelectedColorsChange = ({ target }) => {
+		const newSelectedColors = [...target.selectedOptions].map(
+			(selectedTarget) => selectedTarget.value,
+		);
+		setSelectedColors(newSelectedColors);
 	};
-
-	const { email, login, password } = getState();
-
-	const onChange = ({ target }) => updateState(target.name, target.value);
 
 	return (
 		<div className={styles.app}>
 			<div className={styles.header}>
-				<form onSubmit={onSubmit}>
-					<input
-						type="email"
-						name="email"
-						value={email}
-						placeholder="Email"
-						onChange={onChange}
-					/>
-					<input
-						type="text"
-						name="login"
-						value={login}
-						placeholder="Login"
-						onChange={onChange}
-					/>
-					<input
-						type="password"
-						name="password"
-						value={password}
-						placeholder="Password"
-						onChange={onChange}
-					/>
-					<button type="button" onClick={resetState}>
-						Reset
-					</button>
-					<button type="submit">Submit</button>
-				</form>
+				<select value={selectedProduct} onChange={onSelectedProductChange}>
+					<option value="tv">Television</option>
+					<option value="smartphone">Smartphone</option>
+					<option value="laptop">Laptop</option>
+				</select>
+				<select
+					multiple={true}
+					value={selectedColors}
+					onChange={onSelectedColorsChange}
+				>
+					<option value="black">Black</option>
+					<option value="silver">Silver</option>
+					<option value="white">White</option>
+				</select>
 			</div>
 		</div>
 	);
