211a3910bb93314a05511d164c9b5a2214e3a362
AJ Djiemuratov <lmt9229@yahoo.com>
2023-06-03 10:58:09 +0500

    react-select lib

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,36 +1,28 @@
 import styles from './App.module.css';
-import { useState } from 'react';
+import Select from 'react-select';
+
+const productOptions = [
+	{ value: 'tv', label: 'Television' },
+	{ value: 'smartphone', label: 'Smartphone' },
+	{ value: 'laptop', label: 'Laptop' },
+];
+
+const colorOptions = [
+	{ value: 'black', label: 'Black' },
+	{ value: 'silver', label: 'Silver' },
+	{ value: 'white', label: 'White' },
+];
 
 export const App = () => {
-	const [selectedProduct, setSelectedProduct] = useState('tv');
-	const [selectedColors, setSelectedColors] = useState(['blacf', 'silver']);
-
-	const onSelectedProductChange = ({ target }) => setSelectedProduct(target.value);
-
-	const onSelectedColorsChange = ({ target }) => {
-		const newSelectedColors = [...target.selectedOptions].map(
-			(selectedTarget) => selectedTarget.value,
-		);
-		setSelectedColors(newSelectedColors);
-	};
-
 	return (
 		<div className={styles.app}>
 			<div className={styles.header}>
-				<select value={selectedProduct} onChange={onSelectedProductChange}>
-					<option value="tv">Television</option>
-					<option value="smartphone">Smartphone</option>
-					<option value="laptop">Laptop</option>
-				</select>
-				<select
-					multiple={true}
-					value={selectedColors}
-					onChange={onSelectedColorsChange}
-				>
-					<option value="black">Black</option>
-					<option value="silver">Silver</option>
-					<option value="white">White</option>
-				</select>
+				<Select options={productOptions} defaultValue={productOptions[0]} />
+				<Select
+					isMulti
+					options={colorOptions}
+					defaultValue={[colorOptions[0], colorOptions[1]]}
+				/>
 			</div>
 		</div>
 	);
