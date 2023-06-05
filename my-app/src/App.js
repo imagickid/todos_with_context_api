c405d66778a2b4e51d98d61009be1c3a1e684f12
AJ Djiemuratov <lmt9229@yahoo.com>
2023-06-05 19:51:20 +0500

    input validation

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,42 +1,87 @@
 import styles from './App.module.css';
-import * as yup from 'yup';
-import { yupResolver } from '@hookform/resolvers/yup';
-import { useForm } from 'react-hook-form';
-
-const fieldScheme = yup.object().shape({
-	login: yup
-		.string()
-		.matches(/^[\w_]*$/, 'Use letters, numbers and _.')
-		.max(20, 'Less than 20 symbols.')
-		.min(3, 'More than 3 symbols.'),
-});
+import { useState, useRef } from 'react';
 
 export const App = () => {
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-	} = useForm({
-		defaultValues: {
-			login: '',
-		},
-		resolver: yupResolver(fieldScheme),
-	});
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [confirmPassword, setConfirmPassword] = useState('');
+	const [errorMsg, setErrorMsg] = useState(null);
+	let error = null;
 
-	const loginError = errors.login?.message;
+	const submitButtonRef = useRef(null);
 
-	const onSubmit = (formData) => {
-		console.log(formData);
+	const onEmailChange = ({ target }) => {
+		setEmail(target.value);
+
+		if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(target.value)) {
+			error = 'Invalid email format';
+		}
+		setErrorMsg(error);
+	};
+
+	const onPasswordChange = ({ target }) => {
+		setPassword(target.value);
+
+		if (!/^(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])/.test(target.value)) {
+			error = 'Incorrect Password. Use letters, numbers and symbols';
+		} else if (!/[A-Z]/.test(target.value)) {
+			error = 'Incorrect Password. Include at least 1 capital letter';
+		} else if (!/(?=.{8,20}$)/.test(target.value))
+			error = 'Use minimum 8 and maximum 20 characters';
+		setErrorMsg(error);
+	};
+
+	const onConfirmPasswordChange = ({ target }) => {
+		setConfirmPassword(target.value);
+		if (target.value === password) submitButtonRef.current.focus();
+	};
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		if (password !== confirmPassword) {
+			error = 'Passwords do not match';
+			setErrorMsg(error);
+			setPassword('');
+			setConfirmPassword('');
+		}
+		console.log(email);
+		console.log(password);
+		console.log(confirmPassword);
 	};
 
 	return (
 		<div className={styles.app}>
 			<div className={styles.header}>
-				<form onSubmit={handleSubmit(onSubmit)}>
-					{loginError && <div className={styles.errorLabels}>{loginError}</div>}
-					<input type="text" name="login" {...register('login')} />
-					<button type="submit" disabled={!!loginError}>
-						Send
+				<form className={styles.form} onSubmit={handleSubmit}>
+					{errorMsg && <div className={styles.errorLabels}>{errorMsg}</div>}
+					<input
+						type="email"
+						name="email"
+						value={email}
+						placeholder="johnson@gmail.com"
+						onChange={onEmailChange}
+					/>
+					<input
+						type="password"
+						name="password"
+						placeholder="Password"
+						value={password}
+						onChange={onPasswordChange}
+					/>
+					<input
+						type="password"
+						name="password"
+						placeholder="Confirm  password"
+						value={confirmPassword}
+						onChange={onConfirmPasswordChange}
+					/>
+					<button
+						className={styles.button}
+						ref={submitButtonRef}
+						type="submit"
+						disabled={errorMsg !== null}
+					>
+						Register
 					</button>
 				</form>
 			</div>
