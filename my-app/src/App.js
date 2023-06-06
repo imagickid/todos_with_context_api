f2ef6e365e440d32579aad480671cc899a36839d
AJ Djiemuratov <lmt9229@yahoo.com>
2023-06-06 21:08:50 +0500

    created input form with react_hook_form and yup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,85 +1,85 @@
+import * as yup from 'yup';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 import styles from './App.module.css';
-import { useState, useRef } from 'react';
+import { useEffect, useRef } from 'react';
+
+const fieldSchema = yup.object().shape({
+	email: yup
+		.string()
+		.matches(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Invalid email format'),
+	password: yup
+		.string()
+		.matches(
+			/^(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])/,
+			'Incorrect Password. Use letters, numbers and symbols',
+		)
+		.matches(/[A-Z]/, 'Incorrect Password. Include at least 1 capital letter')
+		.max(20, 'Use maximum 20 characters')
+		.min(8, 'Use minimum 8 characters'),
+	confirmPassword: yup
+		.string()
+		.oneOf([yup.ref('password'), null], 'Passwords do not match'),
+});
+
+const sendFormData = (formData) => {
+	console.log(formData);
+};
 
 export const App = () => {
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
-	const [confirmPassword, setConfirmPassword] = useState('');
-	const [errorMsg, setErrorMsg] = useState(null);
-	let error = null;
+	const {
+		register,
+		getValues,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		defaultValues: { email: '', password: '', confirmPassword: '' },
+		resolver: yupResolver(fieldSchema),
+	});
+
+	const errorMsg = errors.email
+		? errors.email.message
+		: errors.password
+		? errors.password.message
+		: errors.confirmPassword
+		? errors.confirmPassword.message
+		: null;
 
 	const submitButtonRef = useRef(null);
 
-	const onEmailChange = ({ target }) => {
-		setEmail(target.value);
-
-		if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(target.value)) {
-			error = 'Invalid email format';
-		}
-		setErrorMsg(error);
-	};
-
-	const onPasswordChange = ({ target }) => {
-		setPassword(target.value);
-
-		if (!/^(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])/.test(target.value)) {
-			error = 'Incorrect Password. Use letters, numbers and symbols';
-		} else if (!/[A-Z]/.test(target.value)) {
-			error = 'Incorrect Password. Include at least 1 capital letter';
-		} else if (!/(?=.{8,20}$)/.test(target.value))
-			error = 'Use minimum 8 and maximum 20 characters';
-		setErrorMsg(error);
-	};
-
-	const onConfirmPasswordChange = ({ target }) => {
-		setConfirmPassword(target.value);
-		if (target.value === password) submitButtonRef.current.focus();
-	};
-
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		if (password !== confirmPassword) {
-			error = 'Passwords do not match';
-			setErrorMsg(error);
-			setPassword('');
-			setConfirmPassword('');
-		}
-		console.log(email);
-		console.log(password);
-		console.log(confirmPassword);
-	};
+	useEffect(() => {
+		if (getValues('password') === getValues('confirmPassword'))
+			submitButtonRef.current.focus();
+	});
 
 	return (
 		<div className={styles.app}>
 			<div className={styles.header}>
-				<form className={styles.form} onSubmit={handleSubmit}>
+				<form className={styles.form} onSubmit={handleSubmit(sendFormData)}>
 					{errorMsg && <div className={styles.errorLabels}>{errorMsg}</div>}
 					<input
 						type="email"
 						name="email"
-						value={email}
 						placeholder="johnson@gmail.com"
-						onChange={onEmailChange}
+						{...register('email')}
 					/>
 					<input
 						type="password"
 						name="password"
 						placeholder="Password"
-						value={password}
-						onChange={onPasswordChange}
+						{...register('password')}
 					/>
 					<input
 						type="password"
 						name="password"
 						placeholder="Confirm  password"
-						value={confirmPassword}
-						onChange={onConfirmPasswordChange}
+						{...register('confirmPassword')}
 					/>
 					<button
 						className={styles.button}
 						ref={submitButtonRef}
 						type="submit"
-						disabled={errorMsg !== null}
+						disabled={!!errorMsg}
 					>
 						Register
 					</button>
