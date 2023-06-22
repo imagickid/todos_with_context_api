cd5000b828cbd38f890529c7d54a794721f0a8ff
AJ Djiemuratov <lmt9229@yahoo.com>
2023-06-22 21:15:17 +0500

    todos list with jsonplaceholder

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,89 +1,34 @@
-import * as yup from 'yup';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
 import styles from './App.module.css';
-import { useEffect, useRef } from 'react';
-
-const fieldSchema = yup.object().shape({
-	email: yup
-		.string()
-		.matches(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Invalid email format'),
-	password: yup
-		.string()
-		.matches(
-			/^(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])/,
-			'Incorrect Password. Use letters, numbers and symbols',
-		)
-		.matches(/[A-Z]/, 'Incorrect Password. Include at least 1 capital letter')
-		.max(20, 'Use maximum 20 characters')
-		.min(8, 'Use minimum 8 characters'),
-	confirmPassword: yup
-		.string()
-		.oneOf([yup.ref('password'), null], 'Passwords do not match'),
-});
-
-const sendFormData = (formData) => {
-	console.log(formData);
-};
+import { useEffect, useState } from 'react';
 
 export const App = () => {
-	const {
-		register,
-		getValues,
-		handleSubmit,
-		formState: { errors },
-	} = useForm({
-		defaultValues: { email: '', password: '', confirmPassword: '' },
-		resolver: yupResolver(fieldSchema),
-	});
-
-	const errorMsg = errors.email
-		? errors.email.message
-		: errors.password
-		? errors.password.message
-		: errors.confirmPassword
-		? errors.confirmPassword.message
-		: null;
-
-	const submitButtonRef = useRef(null);
+	const [todos, setTodos] = useState([]);
 
 	useEffect(() => {
-		if (getValues('password') === getValues('confirmPassword'))
-			submitButtonRef.current.focus();
-	});
+		try {
+			const fetchPosts = async () => {
+				const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+				const data = await res.json();
+				setTodos(data);
+			};
+			fetchPosts();
+		} catch (err) {}
+	}, []);
 
 	return (
 		<div className={styles.app}>
 			<div className={styles.header}>
-				<form className={styles.form} onSubmit={handleSubmit(sendFormData)}>
-					{errorMsg && <div className={styles.errorLabels}>{errorMsg}</div>}
-					<input
-						type="email"
-						name="email"
-						placeholder="johnson@gmail.com"
-						{...register('email')}
-					/>
-					<input
-						type="password"
-						name="password"
-						placeholder="Password"
-						{...register('password')}
-					/>
-					<input
-						type="password"
-						name="password"
-						placeholder="Confirm  password"
-						{...register('confirmPassword')}
-					/>
-					<button
-						className={styles.button}
-						ref={submitButtonRef}
-						type="submit"
-						disabled={!!errorMsg}
-					>
-						Register
-					</button>
-				</form>
+				<h1>List of things to complete</h1>
+				<div className={styles.label}>
+					{todos.map((todo) => {
+						return (
+							<label key={todo.id}>
+								<span>{todo.id}.</span> {todo.title}
+							</label>
+						);
+					})}
+				</div>
+				;
 			</div>
 		</div>
 	);
